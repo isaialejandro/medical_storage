ed2ff89633b6c15a3c8d28971dfdfd7e83d45505
Isaí Alejandro <isaialejandro26@gmail.com>
2020-09-09 18:54:46 -0500

    gitflow-feature-stash: secrets
    Adding package for secrets:
    
    pip install dotenv
    
    creating .env file for secrets.

--- medical_storage/settings/develop.py
+++ medical_storage/settings/develop.py
@@ -4,23 +4,6 @@ import os
 from .base import *
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-SECRET_KEY = 'l!5g7y24t8_iw9m9sppciq%)9mtoo5)=s+m_!wp*b-c7nw-@)3'
-
-ALLOWED_HOSTS = []   #LocalNetwork
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 #RPI-DB
 DATABASES = {
     'default': {
@@ -34,23 +17,4 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 STATIC_URL = '/static/'
-
--- medical_storage/settings/production.py
+++ medical_storage/settings/production.py
@@ -1,19 +1,6 @@
 from .base import *
 
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-SECRET_KEY = os.environ.get('MEDICAL_APP_SECRET_KEY')
-#SECRET_KEY = 'l!5g7y24t8_iw9m9sppciq%)9mtoo5)=s+m_!wp*b-c7nw-@)3'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG', default=1))
-#DEBUG = False
-
-#ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')   #LocalNetwork 10.64.236.0
-ALLOWED_HOSTS = ['10.64.236.0']
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
